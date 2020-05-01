d8aa1130e5fdbea599c82b826f99f8d5337f1d0b
Aleksandr M <site.rv6@gmail.com>
2020-05-02 00:02:16 +0300

    add project setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
-import VueMeta from 'vue-meta'
 import App from './App.vue'
 
-Vue.use(VueMeta)
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+});
---
+++ vue.config.js
@@ -0,0 +1,50 @@
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const path = require('path');
+
+const faviconImage = path.resolve(__dirname, './project-logo.png');
+const projectImage = path.resolve(__dirname, './project-logo.png');
+
+const faviconPluginOptions = {
+  logo: faviconImage,
+  cache: true,
+  inject: true,
+  mode: 'webapp',
+  devMode: 'webapp',
+  prefix: 'favicon/',
+  background: '#ffffff',
+  theme_color: '#ffffff',
+  favicons: {
+    appName: 'CarRent',
+    icons: {
+      android: true,
+      appleIcon: true,
+      appleStartup: true,
+      favicons: true,
+      firefox: true,
+      coast: true,
+      windows: true,
+      yandex: true,
+    },
+  },
+};
+
+module.exports = {
+  lintOnSave: false,
+  runtimeCompiler: true,
+  productionSourceMap: false,
+  pluginOptions: {
+    notifier: {
+      title: 'CarRent project',
+      alwaysNotify: true,
+      contentImage: projectImage,
+    },
+  },
+  configureWebpack: {
+    plugins: [
+      new FaviconsWebpackPlugin(faviconPluginOptions),
+    ],
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set('~', path.resolve(__dirname));
+  },
+};
