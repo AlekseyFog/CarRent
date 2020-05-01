4eb073056b51ed429740dbfe71c7c2f70aa4cd27
Aleksandr M <site.rv6@gmail.com>
2020-05-02 02:45:08 +0300

    disable form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -117,7 +117,7 @@
         this.formData[field] = event.target.value;
       },
       setTermsValue(event) {
-        this.terms = event.target.value;
+        this.terms = JSON.parse(event.target.value);
       },
       clearFormData() {
         this.formData.name = '';
